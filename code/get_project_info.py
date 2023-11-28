458f4706889dcdc52644d6b5d85da8f32d771aa8
Cong Anh Dung <dungca1512@gmail.com>
2023-11-28 12:33:08 +0700

    get project info for FLC and FRT 23/11/28-12:33:10

--- code/get_project_info.py
+++ code/get_project_info.py
@@ -1,14 +1,16 @@
 import requests
 import json
 
-state = input("Nhập môi trường: ")
+state = input("Enter enviroment: ")
+
+project = input("Enter project: ")
 
 if state == "nlp" or state == "test":
   endpoint = "http://103.176.146.250:5005" # môi trường nlp
 else:
   endpoint = "https://cqe-dev-nlp.fpt.ai/dme" # môi trường dev
 
-url = f"{endpoint}/projects/hc_clx_m3/latest"
+url = f"{endpoint}/projects/{project}/latest"
 
 payload = {}
 headers = {
@@ -17,5 +19,7 @@ headers = {
 
 response = requests.request("GET", url=url, headers=headers, data=payload).json()
 
-with open(f'../json/project_info/m3_project_info_{state}.json', 'w', encoding='utf-8') as json_file:
+with open(f'../project_info/{project}.json', 'w', encoding='utf-8') as json_file:
     json.dump(response, json_file, ensure_ascii=False, indent=4)
+
+print("Done!")
