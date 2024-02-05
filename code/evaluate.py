99b588d20d64d3db90b58a58d64e1b2faa99d26a
Cong Anh Dung <dungca1512@gmail.com>
2024-02-05 16:11:00 +0700

    Debug for FRT 24/02/05-16:10:29

--- code/evaluate.py
+++ code/evaluate.py
@@ -13,8 +13,8 @@ with open(f"../data/transcript_{project}.json", 'r', encoding='utf-8') as f:
 
 project_info = json.load(open(f"../project_info/{project}.json", encoding="utf-8"))["project_info"]
 
-# criteria_names = [i for i in project_info]
-criteria_names = ["greet"]
+criteria_names = [i for i in project_info]
+# criteria_names = ["greet"]
 project_info_eval = {}
 for criteria in criteria_names:
     project_info_eval.update({criteria: project_info[criteria]})
